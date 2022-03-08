9cb2f5572f8321ae9eb5d37df82b41bbaa019c7f
nigris <gear.ware@gmx.fr>
2022-03-08 12:44:12 +0100

    functionVerifPasswordInscription
    With this function, we check if the password of the user is not to much easy

---
+++ js/functionVerifPaswordInscription.js
@@ -0,0 +1,30 @@
+
+    function isInAlphabet(letter){
+        for (let i=1; i<27;i++){
+            if (letter==((i + 9).toString(36))) {
+                return true ;
+            }
+        }
+        return false;
+
+    }
+
+    
+    function isPasswordToMuchEasy(passwordIdName){
+
+        let password=document.getElementById(passwordIdName).value;
+        let minPassLength=10;
+
+        if (password.length < minPassLength){           
+            return true;
+        }
+        for (let letter in password) {
+            if (isInAlphabet(password[letter])==false){
+                return false;
+            }
+        }
+        
+        return true;
+        
+
+    }
