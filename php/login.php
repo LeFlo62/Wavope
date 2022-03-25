702a1557049a39ccfab8d14f98a0aacff9b243c2
LeFlo <leflodu62@live.fr>
2022-03-25 11:26:40 +0100

    Started php back_end

---
+++ php/login.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	
+	include_once 'mysql.php';
+	
+	function login($username, $password){
+		$bdh = new DBHandler();
+		
+		if (empty($username) OR empty($password)){
+			return 'ER_empty';
+		}
+		
+		$requser = $bdh->getInstance()->prepare("SELECT * FROM users WHERE username = ?");
+		$requser->execute(array($username));
+		$userexist = $requser->rowCount();
+		
+		if($userexist == 1){
+			$userinfo = $requser->fetch();
+			if(password_verify($password, $userinfo['password'])){
+				$_SESSION['id'] = $userinfo['id'];
+				$_SESSION['username'] = $userinfo['username'];
+
+				return 'CONNECTED';
+			} else {
+				return 'ER_password';
+			}
+		} else {
+			return 'ER_unknown';
+		}
+	}
+?>
---
+++ php/mysql.php
@@ -0,0 +1,27 @@
+<?php 
+	class DBHandler{
+		private $db;
+		
+		function __construct(){
+			$this->connect();
+		}
+		
+		private function connect(){
+			try {
+				$host = 'mysql:host=127.0.0.1;dbname=wavope;charset=utf8';
+				$params = array(
+						PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+						PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+				);
+				
+				$this->db = new PDO($host, 'wavope', 'wavope_password!', $params);
+			} catch (PDOException $e) {
+				die('Erreur de connexion à la base de donné !');
+			}
+		}
+		
+		public function getInstance(){
+			return $this->db;
+		}
+	}
+?>
