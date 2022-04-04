44f5299d9b8803772a20512a9bdc460a57a38987
nigris <gear.ware@gmx.fr>
2022-04-04 17:47:18 +0200

    centerWithFlexboxTheFormBlock

--- login.php
+++ login.php
@@ -13,9 +13,11 @@
     <body>	
         <?php include 'navbar.php'; ?>
         <svg class="svgWave" viewBox="0 0 1440 320"><path class="svgWaveCaracteristique"  d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,229.3C672,213,768,139,864,96C960,53,1056,43,1152,48C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-<h2>CONNECTEZ-VOUS</h2>
+
         <div class="box">
+            
             <div class="blockFormulaire">
+                <h1>CONNECTEZ-VOUS</h1>
                 <form action="#" method="post">
                     
                     <div class="blockTextInput">
