560dbd50f19dfd4e96d05245491c9078b1e4bab5
Raphaël <46501302+Raph1321@users.noreply.github.com>
2022-05-02 16:52:57 +0200

    test

--- about.php
+++ about.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -57,97 +56,12 @@
     <div class="blockProfil">
         <div class="blockProfilImage">
             <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/Maxime.png" alt="image 2" class="imageProfil"></a>
-=======
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="./css/styleAbout.css">
-    </head>
-
-    <body>
-        <?php include 'navbar.php' ?>
-
-        <section class="blockAll">
-            <div class="topPage">
-                <svg class="svgWave" viewBox="0 0 1440 320"><path class="svgWaveCaracteristique"  d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,229.3C672,213,768,139,864,96C960,53,1056,43,1152,48C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-            </div>
-
-            <div class="blockTitre">
-                <h1>Présentation de la team</h1>
-            </div>
-            <div class="presTeam">
-                <p class="presTeamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel dolor aliquam, tincidunt urna at, tincidunt lorem. Aenean velit felis, pharetra a varius quis, rhoncus blandit ante. Vivamus ullamcorper tempus nisi, nec bibendum lacus facilisis in. In sodales malesuada sagittis. Nunc venenatis nulla eu mauris porta, id semper justo interdum. Fusce pharetra ipsum at arcu porta, ut sollicitudin dolor mattis. In ultrices dolor fringilla turpis gravida maximus. Donec sit amet est dapibus, tempus eros vel, vehicula felis. Nunc quis elementum massa. Praesent eu elit vitae libero fermentum maximus ac ac nisl. Vestibulum vulputate ipsum erat. Nullam vitae eros id dolor pulvinar tincidunt. Vivamus sed dui scelerisque, finibus leo et, dignissim ante.</p>
-            </div>
-
-            <div class="blockProfil">
-                <div class="blockProfilImage">
-                    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2" class="imageProfil"></a>
-                </div>
-                <div class="blockProfilDescription">
-                    <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
-                </div>
-            </div>
-
-            <div class="blockProfil">
-                <div class="blockProfilImage">
-                    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2" class="imageProfil"></a>
-                </div>
-                <div class="blockProfilDescription">
-                    <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
-                </div>
-            </div>
-
-            <div class="blockProfil">
-                <div class="blockProfilImage">
-                    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2" class="imageProfil"></a>
-                </div>
-                <div class="blockProfilDescription">
-                    <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
-                </div>
-            </div>
-
-            <div class="blockProfil">
-                <div class="blockProfilImage">
-                    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2" class="imageProfil"></a>
-                </div>
-                <div class="blockProfilDescription">
-                    <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
-                </div>
-            </div>
-
-            <div class="blockProfil">
-                <div class="blockProfilImage">
-                    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2" class="imageProfil"></a>
-                </div>
-                <div class="blockProfilDescription">
-                    <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
-                </div>
-            </div>
-
-
-            <div class="blockTitre">
-            <h1>Présentation du projet</h1>
-            </div>
-            <div class="presTeam">
-                <p class="presTeamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel dolor aliquam, tincidunt urna at, tincidunt lorem. Aenean velit felis, pharetra a varius quis, rhoncus blandit ante. Vivamus ullamcorper tempus nisi, nec bibendum lacus facilisis in. In sodales malesuada sagittis. Nunc venenatis nulla eu mauris porta, id semper justo interdum. Fusce pharetra ipsum at arcu porta, ut sollicitudin dolor mattis. In ultrices dolor fringilla turpis gravida maximus. Donec sit amet est dapibus, tempus eros vel, vehicula felis. Nunc quis elementum massa. Praesent eu elit vitae libero fermentum maximus ac ac nisl. Vestibulum vulputate ipsum erat. Nullam vitae eros id dolor pulvinar tincidunt. Vivamus sed dui scelerisque, finibus leo et, dignissim ante.</p>
-            </div>
-        </section>
-
-
-
-        <div class="Images">
-            <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2"></a>
-            <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 3.jpg" alt="image 3"></a>
-            <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 4.jpg" alt="image 4"></a>
-            <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 5.jpg" alt="image 5"></a>
-            <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 6.jpg" alt="image 6"></a>
->>>>>>> bf0a4fa4dbb23a826f8615bb7e0802e26ad863ee
         </div>
+        <div class="blockProfilDescription">
+            <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
         </div>
+    </div>
 
-<<<<<<< HEAD
     <div class="blockProfil">
         <div class="blockProfilImage">
             <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/Hugo.jpg" alt="image 2" class="imageProfil"></a>
@@ -163,9 +77,30 @@
         </div>
         <div class="blockProfilDescription">
             <p>lorem ipsum amet, consectetur adipiscing elit. Integer vel</p>
-=======
->>>>>>> bf0a4fa4dbb23a826f8615bb7e0802e26ad863ee
         </div>
+    </div>
 
-    </body>
+
+    <div class="blockTitre">
+    <h1>Présentation du projet</h1>
+    </div>
+    <div class="presTeam">
+        <p class="presTeamText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel dolor aliquam, tincidunt urna at, tincidunt lorem. Aenean velit felis, pharetra a varius quis, rhoncus blandit ante. Vivamus ullamcorper tempus nisi, nec bibendum lacus facilisis in. In sodales malesuada sagittis. Nunc venenatis nulla eu mauris porta, id semper justo interdum. Fusce pharetra ipsum at arcu porta, ut sollicitudin dolor mattis. In ultrices dolor fringilla turpis gravida maximus. Donec sit amet est dapibus, tempus eros vel, vehicula felis. Nunc quis elementum massa. Praesent eu elit vitae libero fermentum maximus ac ac nisl. Vestibulum vulputate ipsum erat. Nullam vitae eros id dolor pulvinar tincidunt. Vivamus sed dui scelerisque, finibus leo et, dignissim ante.</p>
+    </div>
+</section>
+
+
+
+<div class="Images">
+    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 2.jpg" alt="image 2"></a>
+    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 3.jpg" alt="image 3"></a>
+    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 4.jpg" alt="image 4"></a>
+    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 5.jpg" alt="image 5"></a>
+    <a href="https://www.linkedin.com/in/raphael-gradus/"><img src="./Images/image random 6.jpg" alt="image 6"></a>
+</div>
+</div>
+
+</div>
+
+</body>
 </html>
