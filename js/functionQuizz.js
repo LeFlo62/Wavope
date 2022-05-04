258f80ec874775314113c954118a2dbff04b770d
nigris <92745772+nigris@users.noreply.github.com>
2022-05-04 03:13:02 +0200

    Update functionQuizz.js

--- js/functionQuizz.js
+++ js/functionQuizz.js
@@ -4,7 +4,7 @@ const questions=["Qu’est-ce qui peut faire le tour du monde en restant dans so
 const answers= [["Un timbre","lorem2","lorem3"],["La migraine","lorem2","lorem3","lorem4"],["Un champignon","lorem2","lorem3"]]
 
 function nextStepQuizz(){
-    document.getElementById("percentage").innerHTML=step/(questions.length) +"%";
+    document.getElementById("percentage").innerHTML=100*step/(questions.length) +"%";
     document.documentElement.style.setProperty('--h', 100*step/(questions.length)+ '%')
     if (step/(questions.length)==1){
         newQuestion("Vous avez terminé le quizz",[]);
@@ -19,7 +19,6 @@ function newQuestion(question,answers){
     document.getElementById("answerBlock").innerHTML = '';
         document.getElementById("question").innerHTML=question;
         for(var i=0;i<answers.length;i++){
-            console.log(i);
             let  newAnswer = document.createElement("div");
             newAnswer.classList.add("answer");
             let newContent = document.createTextNode(answers[i]);
