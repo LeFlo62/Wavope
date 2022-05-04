e5788ac8360bdf65ebbb768f68da55d11a426fef
LeFlo <leflodu62@live.fr>
2022-05-04 16:14:34 +0200

    Changed db location

--- php/mysql.php
+++ php/mysql.php
@@ -8,13 +8,13 @@
 		
 		private function connect(){
 			try {
-				$host = 'mysql:host=127.0.0.1;dbname=wavope;charset=utf8';
+				$host = 'mysql:host=149.202.75.88;port=3306;dbname=wavope;charset=utf8';
 				$params = array(
 						PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 						PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 				);
 				
-				$this->db = new PDO($host, 'wavope', 'wavope_password!', $params);
+				$this->db = new PDO($host, 'wavope', '07modIXrejFX9d!c0d^ysb9WHAH#daBw', $params);
 			} catch (PDOException $e) {
 				die('Erreur de connexion à la base de données ! '. $e->getMessage());
 			}
--- php/variables.php
+++ php/variables.php
@@ -1,3 +1,3 @@
 <?php
-define('RANK_POWER', array('user' => 0, 'collaborateur' => 1, 'admin' => 2));
+define('RANK_POWER', array('user' => 0, 'collaborateur' => 1, 'admin' => 2, 'superadmin' => 1000));
 ?>
