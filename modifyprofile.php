80f1d9aac09e23d7c541f8ab6b56f5a79c186491
Flawl3ss04 <hugopabich04@gmail.com>
2022-05-05 19:26:30 +0200

    Started PHP on modifyprofile

--- modifyprofile.php
+++ modifyprofile.php
@@ -16,6 +16,7 @@
     $requserdata->execute();
     $userdata = $requserdata->fetch();
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -31,38 +32,46 @@
 	<svg class="svgWave" viewBox="0 0 1440 320"><path class="svgWaveCaracteristique"  d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,229.3C672,213,768,139,864,96C960,53,1056,43,1152,48C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 
 	<div class="blockInformation">
-            <div class="information">
-                <h1>GERER SON PROFIL</h1>
-                <form class="profileForm" action="#" method="post">
+        <div class="information">
+            <h1>GERER SON PROFIL</h1>
+            <form class="profileForm" action="#" method="post">
                     
-                    <div class="blockText">
-                        <label for="firstnameField">Prénom</label>
-                        <input class="fieldInput" type="text" id="firstname" name="firstname" required placeholder="Jean" value="<?php
+                <div class="blockText">
+                    <label for="firstnameField">Prénom</label>
+                    <input class="fieldInput" type="text" id="firstname" name="firstname" required placeholder="Jean" value="<?php
                                 echo $userdata['firstname'];
-                            ?>">
-                    </div>
+                        ?>">
+                </div>
 
 
-                    <div class="blockText">
-                        <label for="lastnameField">Nom</label>
-                        <input class="fieldInput" type="text" id="lastname" name="lastname" required placeholder="Dupont" value="<?php
+                <div class="blockText">
+                    <label for="lastnameField">Nom</label>
+                    <input class="fieldInput" type="text" id="lastname" name="lastname" required placeholder="Dupont" value="<?php
                                 echo $userdata['lastname'];
-                            ?>">
-                    </div>
+                        ?>">
+                </div>
 
-                    <div class="blockText">
-                        <label for="birthdateField">Date de naissance</label>
-                        <input class="fieldInput" type="date" id="birthdate" name="birthdate" value="<?php
+                <div class="blockText">
+                    <label for="birthdateField">Date de naissance</label>
+                    <input class="fieldInput" type="date" id="birthdate" name="birthdate" value="<?php
                                 echo $userdata['birthdate'];
-                            ?>" required>
-                    </div>
+                        ?>" required>
+                </div>
 
-                    <div class="button">
-                        <input type="submit" name="buttonModified" class="buttonModif" value="MODIFIER"/>
-                    </div>
-                </form> 
-            </div>
-		</div>
+                <div class="button">
+                    <input type="submit" name="buttonModified" class="buttonModif" value="MODIFIER"/>
+                </div>
+            </form> 
+        </div>
+	</div>
+
+    <!-- <?php
+        if(isset($_POST('MODIFIER'))){
+
+        }
+    ?>
+    -->
+
 
 </body>
 
