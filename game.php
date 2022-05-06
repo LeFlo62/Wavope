9af0e37306f0dd656ca8de32d9585337553cd96f
nigris <92745772+nigris@users.noreply.github.com>
2022-05-06 02:33:53 +0200

    add playerSprite and update the game

--- game.php
+++ game.php
@@ -11,9 +11,6 @@
 </head>
 <body>
     <?php include 'navbar.php' ?>
-    <svg class="svgWave" viewBox="0 0 1440 320"><path class="svgWaveCaracteristique"  d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,229.3C672,213,768,139,864,96C960,53,1056,43,1152,48C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-
-
     <section class="blockAllQuizz">
         <div class="blockQuizz">
             <div class="progress-container">
@@ -32,7 +29,7 @@
             <div class="blockPoint">
                 vous avez : <span id="point">0</span> point(s)
             </div>
-            <img id="playerSprite" src="" alt="playerSprite">
+            <img id="playerSprite" src="./Images/gameSprite/playerSprite.png" alt="playerSprite">
             <img id="obstacle" src="./Images/gameSprite/plastic.png">
         </div>
     </section>
--- js/gameFunctions.js
+++ js/gameFunctions.js
@@ -1,53 +1,68 @@
 var isRunning=false; 
-        var startTime;
-        console.log(isRunning);
-        document.addEventListener('keyup', event => {
-        if ( event.code === 'Space' && isRunning==false) {
-            console.log('start');
-            startGame(); 
-        }
-        if (event.code === 'Space' && isRunning==true) {
-            jump("playerSprite");
-        }
-    })
+var startTime;
+console.log(isRunning);
+document.addEventListener('keydown', event => {
+if ( event.code === 'Space' && isRunning==false) {
+    console.log('start');
+    startGame(); 
+}
+if (event.code === 'Space' && isRunning==true) {
+    jump("playerSprite");
+}
+if (event.key === 'ArrowLeft') {
+    leftMove("playerSprite");
+}
+if (event.key === 'ArrowRight') {
+    rightMove("playerSprite");
+    }
 
-    function displayPoint(){
-            console.log(document.getElementById("point").innerHTML= (Date.now() - startTime));
-            if (isRunning==true){
-                setInterval( function() { displayPoint(); }, 10 );
-            }   
-            else{
-                document.getElementById("point").innerHTML= 0;
-            }
-        }
+})
 
-        function jump(idName){            
-            document.getElementById(idName).animate([
-                { transform: 'translateY(0px)' },
-                { transform: 'translateY(-80px)' },
-                { transform: 'translateY(-140px)' },
-                { transform: 'translateY(-80px)' },
-                { transform: 'translateY(0px)' }
-                ], {
-                duration: 600,
-                });
-        }
-
-    function isCollapsed(firstIdName,secondIdName){
-        
-        let firstElement = document.getElementById(firstIdName).getBoundingClientRect();
-        let secondElement = document.getElementById(secondIdName).getBoundingClientRect();
-        if (firstElement.left + firstElement.width > secondElement.left &&
-        firstElement.left < secondElement.left + secondElement.width &&
-        firstElement.top < secondElement.top + secondElement.height &&
-        firstElement.height + firstElement.top > secondElement.top) {
-            document.getElementById("obstacle").style.animation="none";
-            stopGame();
-            alert("Game over");
-        }
+function displayPoint(){
+        document.getElementById("point").innerHTML= (Date.now() - startTime);
+        if (isRunning==true){
+            setInterval( function() { displayPoint(); }, 10 );
+        }   
         else{
-            setInterval( function() { isCollapsed(firstIdName,secondIdName); }, 10 );
+            document.getElementById("point").innerHTML= 0;
         }
+    }
+
+    function jump(idName){      
+        document.getElementById(idName).animate([
+            { transform: 'translateY(0px)' },
+            { transform: 'translateY(-80px)' },
+            { transform: 'translateY(-140px)' },
+            { transform: 'translateY(-80px)' },
+            { transform: 'translateY(0px)' }
+            ], {
+            duration: 600,
+            });
+    }
+
+    function rightMove(idName){
+        console.log("right");
+    }
+
+    function leftMove(idName){
+        console.log("left");
+    }
+
+function isCollapsed(firstIdName,secondIdName){
+    
+    let firstElement = document.getElementById(firstIdName).getBoundingClientRect();
+    let secondElement = document.getElementById(secondIdName).getBoundingClientRect();
+    if (firstElement.left + firstElement.width > secondElement.left &&
+    firstElement.left < secondElement.left + secondElement.width &&
+    firstElement.top < secondElement.top + secondElement.height &&
+    firstElement.height + firstElement.top > secondElement.top) {
+        document.getElementById("obstacle").style.animation="none";
+        stopGame();
+        alert("Game over: arretez d'utiliser des bouteilles en plastiques !!");
+    }
+    else{
+        setInterval( function() { isCollapsed(firstIdName,secondIdName); }, 10 );
+    }
 }
 
 function startGame(){
@@ -56,7 +71,6 @@ function startGame(){
     document.getElementById("obstacle").style.animation="obstacleAnimation 2s linear infinite";
     displayPoint();
     isCollapsed("playerSprite","obstacle");
-    
 }
 
 function stopGame(){
