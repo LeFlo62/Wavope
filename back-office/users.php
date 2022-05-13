0a1bdfcfc66db0c100d3bd277ce59d87d44c18f9
LeFlo <leflodu62@live.fr>
2022-05-13 16:48:39 +0200

    Implemented back-end for user removal and banishment

--- back-office/users.php
+++ back-office/users.php
@@ -85,15 +85,49 @@
             $('#modal-background').fadeIn();
 
             $('#modal').css({'display': 'flex'})
-            .html('<p class="modal-title" style="color: red;">Suppression ?</p><p>Voulez-vous vraiment supprimer ce compte ?</p><div class="buttons"><div class="accept">Confirmer</div><div class="cancel">Annuler</div></div>')
+            .html('<p class="modal-title" style="color: red;">Suppression ?</p><p>Voulez-vous vraiment supprimer ce compte ?</p><div class="buttons"><div class="accept" action="delete" user-id=' + $(this).attr("user-id") + '>Confirmer</div><div class="cancel">Annuler</div></div>')
             .animate({'opacity': '1'});
         });
 
-        $('div.cancel').click(function(){
+        $('.ban').click(function(){
+            $('#modal-background').fadeIn();
+
+            $('#modal').css({'display': 'flex'})
+            .html('<p class="modal-title" style="color: red;">Bannissement ?</p><p>Voulez-vous vraiment bannir ce compte ?</p><div class="buttons"><div class="accept" action="ban" user-id=' + $(this).attr("user-id") + '>Confirmer</div><div class="cancel">Annuler</div></div>')
+            .animate({'opacity': '1'});
+        });
+
+        $(document).on('click', '.accept', function(){
+            var btn = $(this);
             $('#modal-background').fadeOut();
             $('#modal').animate({'opacity': '0'}, function(){
                 $("#modal").css({'display': 'none'});
-                alert("finish");
+            });
+
+            var userId = btn.attr('user-id');
+            var action = btn.attr('action');
+
+            $.post("./back-office/php/sanctionuser.php", {user_id: userId, action: action})
+                .done(function(response){
+                    var responseObj = JSON.parse(response);
+                    $('#snackbar').html(responseObj.message).addClass(['show', responseObj.return_type]);
+                    setTimeout(function(){
+                        $('#snackbar').removeClass(['show', responseObj.return_type]);
+                    }, 3000);
+
+                    if(responseObj.return_type == 'success'){
+                        //TODO: enlever la ligne, la rayer ou la marquer comme bannie.
+                    }
+                })
+                .fail(function(){
+                    alert("error");
+                });
+        });
+
+        $(document).on('click', '.cancel', function(){
+            $('#modal-background').fadeOut();
+            $('#modal').animate({'opacity': '0'}, function(){
+                $("#modal").css({'display': 'none'});
             });
         });
 
