14151d30cc92a680b0847afd6d0910df41d33172
Flawl3ss04 <hugopabich04@gmail.com>
2022-05-17 22:40:21 +0200

    Finished the style for the CGU page

--- cgu.php
+++ cgu.php
@@ -5,17 +5,17 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/styleProfil.css"/>
+        <link rel="stylesheet" href="./css/styleCgu.css"/>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     </head>
 
     <body>
-    	<div class="box">
+    	<?php include 'navbar.php' ?>
+            <svg class="svgWave" viewBox="0 0 1440 320"><path class="svgWaveCaracteristique"  d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,229.3C672,213,768,139,864,96C960,53,1056,43,1152,48C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 
-    		<?php include 'navbar.php' ?>
-            	<svg class="svgWave" viewBox="0 0 1440 320"><path class="svgWaveCaracteristique"  d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,229.3C672,213,768,139,864,96C960,53,1056,43,1152,48C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        <div class="box">
 
        		<div class="big_title"> 
         		Politique de confidentialité de Wavope
@@ -23,17 +23,21 @@
 
         	<div class="text">
 				Pour recevoir des informations sur les Données personnelles, les objectifs et les parties avec qui elles sont partagées, veuillez contacter le Propriétaire.
-			</div>
+			</div><br>
+
+			<div id="trait"></div><br>
 
 			<div class="title">
 				Propriétaire et Responsable du traitement
-			</div>
+			</div>		
 
 			<div class="text">
-				Infinite Measures - 55 Rue du Faubourg Saint-Honoré - 75008 Paris
+				Infinite Measures - 55 Rue du Faubourg Saint-Honoré - 75008 Paris <br>
 
 				Courriel de contact du Propriétaire : infinitemeasuresfr@gmail.com
-			</div>
+			</div><br>
+
+			<div id="trait"></div><br>
 
 			<div class="title">
 				Types de Données collectées
@@ -49,11 +53,13 @@
 				Toute utilisation des Cookies – ou d’autres outils de suivi – par cette Application ou par les propriétaires de services tiers utilisés par cette Application vise à fournir le Service demandé par l’Utilisateur, outre les autres finalités décrites dans le présent document et dans la Politique relative aux cookies, si elle est disponible.
 
 				Les Utilisateurs sont responsables de toute Donnée personnelle de tiers obtenue, publiée ou communiquée par l’intermédiaire de cette Application et confirment qu’ils obtiennent le consentement du tiers pour fournir les Données au Propriétaire.
-			</div>
+			</div><br>
+
+			<div id="trait"></div><br>
 
 			<div class="title">
 				Mode et lieu de traitement des Données
-			</div>
+			</div><br>
 
 			<div class="little_title">
 				Méthodes de traitement
@@ -62,7 +68,7 @@
 			<div class="text">
 				Le Propriétaire prend les mesures de sécurité appropriées afin d’empêcher l’accès, la divulgation, la modification ou la destruction non autorisés des Données.
 				Le traitement des Données est effectué à l’aide d’ordinateurs ou d’outils informatiques, en suivant les procédures et les modes organisationnels étroitement liés aux finalités indiquées. Outre le Propriétaire, les Données peuvent être accessibles, dans certains cas, à certaines catégories de personnes en charge du fonctionnement de cette Application (administration, ventes, marketing, service juridique, administration du système) ou à des parties externes (telles que les fournisseurs tiers de services techniques, les services de messagerie, les fournisseurs d’hébergement, les entreprises informatiques, les agences de communication) désignées, le cas échéant, comme Sous-traitantes par le Propriétaire. La liste mise à jour de ces parties peut être demandée à tout moment au Propriétaire.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Base juridique du traitement
@@ -78,7 +84,7 @@
 				<li> le traitement est nécessaire aux fins des intérêts légitimes poursuivis par le Propriétaire ou par un tiers. </li>
 			</ul>
 				Dans tous les cas, le Propriétaire vous aidera volontiers à clarifier la base juridique spécifique qui s'applique au traitement, et en particulier si la fourniture de Données personnelles est une exigence légale ou contractuelle, ou une exigence nécessaire pour conclure un contrat.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Lieu de traitement
@@ -91,7 +97,7 @@
 				Les Utilisateurs ont également le droit de connaître la base juridique des transferts de Données vers un pays situé en dehors de l'Union européenne ou vers toute organisation internationale régie par le droit international public ou créée par deux pays ou plus, comme l'ONU, ainsi que les mesures de sécurité prises par le Propriétaire pour sauvegarder leurs Données.
 
 				Si un tel transfert a lieu, les Utilisateurs peuvent en savoir plus en consultant les sections correspondantes du présent document ou se renseigner auprès du Propriétaire en utilisant les informations fournies dans la section de contact.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Temps de conservation
@@ -108,7 +114,9 @@
 				Le Propriétaire peut être autorisé à conserver des Données personnelles plus longtemps chaque fois que l’Utilisateur a donné son consentement à un tel traitement, tant que ce consentement n’est pas retiré. En outre, le Propriétaire peut être obligé de conserver des Données personnelles plus longtemps chaque fois que cela est requis pour l'exécution d'une obligation légale ou sur ordre d'une autorité.
 
 				Une fois la période de conservation expirée, les Données personnelles seront supprimées. Par conséquent, le droit d'accès, le droit d'effacement, le droit de rectification et le droit à la portabilité des données ne peuvent être appliqués après l'expiration de la période de conservation.
-			</div>
+			</div><br>
+
+			<div id="trait"></div><br>
 
 			<div class="title">
 				Droits des Utilisateurs
@@ -128,7 +136,7 @@
 				<li> Récupérer leurs Données et les transférer à un autre responsable du traitement. Les Utilisateurs ont le droit de récupérer leurs Données dans un format structuré, couramment utilisé et lisible par machine et, si cela est techniquement possible, de les transmettre à un autre responsable du traitement sans obstacle d'aucune sorte. Cette disposition s’applique, sous réserve que les Données soient traitées par des moyens automatisés et que le traitement repose sur le consentement de l'Utilisateur, sur un contrat auquel l'Utilisateur est partie ou sur des obligations précontractuelles. </li>
 				<li> Déposer plainte. Les Utilisateurs ont le droit de déposer une plainte auprès de leur autorité compétente en matière de protection des données. </li>
 			</ul>
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Informations concernant le droit d'opposition au traitement
@@ -138,7 +146,7 @@
 				Lorsque les Données personnelles sont traitées dans l'intérêt public, dans l'exercice d'une autorité officielle dévolue au Propriétaire ou aux fins des intérêts légitimes poursuivis par celui-ci, les Utilisateurs peuvent s'opposer à ce traitement en fournissant un motif lié à leur situation particulière devant justifier cette opposition.
 
 				Les Utilisateurs doivent cependant savoir que si leurs Données personnelles sont traitées à des fins de marketing direct, ils peuvent s'opposer à ce traitement à tout moment sans aucune justification. Pour savoir si le Propriétaire traite des Données personnelles à des fins de marketing direct, les Utilisateurs peuvent se reporter aux sections correspondantes du présent document.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Comment exercer ces droits
@@ -146,11 +154,13 @@
 
 			<div class="text">
 				Toute demande d'exercice des droits de l'Utilisateur peut être adressée au Propriétaire grâce aux coordonnées fournies dans le présent document. Ces demandes peuvent être exercées gratuitement et seront étudiées par le Propriétaire le plus tôt possible et toujours dans un délai d'un mois.
-			</div>
+			</div><br>
+
+			<div id="trait"></div><br>
 
 			<div class="title">
 				Informations supplémentaires sur le traitement et la collecte des Données
-			</div>
+			</div><br>
 
 			<div class="little_title">
 				Action en justice
@@ -159,7 +169,7 @@
 			<div class="text">
 				Les Données personnelles de l’Utilisateur peuvent être utilisées à des fins juridiques par le Propriétaire devant les tribunaux ou dans les étapes pouvant conduire à une action en justice résultant d’une utilisation inappropriée de cette Application ou des Services connexes.
 				L’Utilisateur est conscient du fait que le Propriétaire peut être amené à révéler des Données personnelles à la demande des autorités publiques.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Informations supplémentaires concernant les Données personnelles de l’Utilisateur
@@ -167,7 +177,7 @@
 
 			<div class="text">
 				Outre les informations contenues dans la présente politique de confidentialité, cette Application peut fournir à l’Utilisateur des renseignements complémentaires et des informations contextuelles concernant des services particuliers ou la collecte et le traitement des Données personnelles.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Journaux système et maintenance
@@ -175,7 +185,7 @@
 
 			<div class="text">
 				À des fins d'exploitation et de maintenance, cette Application et tout service tiers peuvent collecter des fichiers qui enregistrent les interactions avec cette Application (journaux système) ou utiliser à cette fin d'autres Données personnelles (telles que l'adresse IP).
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Informations non incluses dans la présente politique
@@ -183,7 +193,7 @@
 
 			<div class="text">
 				De plus amples renseignements concernant la collecte ou le traitement des Données personnelles peuvent à tout moment être demandés au Propriétaire. Veuillez consulter les coordonnées figurant au début du présent document.
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Comment les demandes « Ne pas pister » sont traitées
@@ -192,7 +202,7 @@
 			<div class="text">
 				Cette Application ne prend pas en charge les demandes « Ne pas pister ».
 				Référez-vous à la politique de confidentialité des services tiers pour déterminer s’ils acceptent ou non aux demandes « Ne pas pister ».
-			</div>
+			</div><br><br>
 
 			<div class="little_title">
 				Modifications de la présente politique de confidentialité
@@ -202,7 +212,9 @@
 				Le Propriétaire se réserve le droit d'apporter des modifications à la présente politique de confidentialité, à tout moment, en informant ses Utilisateurs sur cette page et éventuellement dans cette cette Application ou – pour autant que cela soit techniquement et légalement possible – en envoyant une notification aux Utilisateurs par l'intermédiaire des coordonnées disponibles pour le Propriétaire. Il est fortement recommandé de consulter cette page fréquemment, en se référant à la date de la dernière modification indiquée en bas.
 
 				Si les modifications influencent les activités de traitement effectuées sur la base du consentement de l'Utilisateur, le Propriétaire doit recueillir un nouveau consentement de l'Utilisateur lorsque nécessaire.
-			</div>
+			</div><br>
+
+			<div id="trait"></div><br>
 
 			<div class="title">
 				Informations légales
@@ -212,7 +224,7 @@
 				La présente politique de confidentialité a été préparée en exécution des dispositions de plusieurs législations, notamment de l'article 13/14 du règlement européen 2016/679 (règlement général sur la protection des données).
 
 				Cette politique de confidentialité concerne uniquement cette Application, sauf indication contraire dans le présent document.
-			</div>
+			</div><br><br>
 		</div>
 	</body>
 </html>
