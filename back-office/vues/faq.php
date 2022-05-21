0922f9ff9d383b41694d580060c8968169e7e743
LeFlo <leflodu62@live.fr>
2022-05-21 19:29:08 +0200

    Finished faq question switching order animation

--- back-office/vues/faq.php
+++ back-office/vues/faq.php
@@ -6,7 +6,7 @@
     foreach($faq as $qa){
         $ordering = $qa['ordering'];
         echo '<div qa-id="'. $qa['id'] .'" class="question-answer">
-                <p class="question">'. $qa['question'] . ($ordering === count($faq)-1 ? '' : '<i class="control fa-solid fa-caret-down"></i>') . ($ordering === 0 ? '' : '<i class="control fa-solid fa-caret-up"></i>') .'</p>
+                <p class="question">'. $qa['question'] . '<i class="control fa-solid fa-caret-down" '. ($ordering === count($faq)-1 ? 'style="display:none;"' : '') .'></i><i class="control fa-solid fa-caret-up" '. ($ordering === 0 ? 'style="display: none;"' : '') . '></i></p>
                 <p class="answer">'. $qa['answer'] .'</p>
             </div>';
     }
@@ -22,7 +22,7 @@
         var switchElement = up ? elem.prev() : elem.next();
         var switchId = switchElement.attr('qa-id');
 
-        var height = switchElement.outerHeight(true);
+        var height = (up ? 1 : -1) * switchElement.outerHeight(true);
 
         elem.css({'box-shadow': '0 0 12px 0 grey'});
 
@@ -30,14 +30,30 @@
 
         switchElement.animate({'top': height.toString() + 'px'}, function(){
             switchElement.css({'top': ''});
-            elem.after(switchElement);
-            elem.css({'box-shadow': ''});
         });
 
         elem.animate({'top': (-height).toString() + 'px'}, function(){
             elem.css({'top': ''});
-            switchElement.before(elem);
+
+            if(up){
+                switchElement.before(elem);
+            } else {
+                switchElement.after(elem);
+            }
             elem.css({'box-shadow': ''});
+            if(elem.attr('qa-id') === $('.question-answer:' + (up ? 'first' : 'last')).attr('qa-id')){
+                elem.find('.fa-caret-' + (up ? 'up' : 'down') + '').hide();
+            } else{
+                elem.find('.fa-caret-up').show();
+                elem.find('.fa-caret-down').show();
+            }
+
+            if(switchElement.attr('qa-id') === $('.question-answer:' + (up ? 'last' : 'first')).attr('qa-id')){
+                switchElement.find('.fa-caret-' + (up ? 'down' : 'up') + '').hide();
+            } else{
+                switchElement.find('.fa-caret-up').show();
+                switchElement.find('.fa-caret-down').show();
+            }
         });
     });
 </script>
