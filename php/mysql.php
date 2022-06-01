7344918b259ff2fb6af6469b2a6824ae99a7b37d
LeFlo <leflodu62@live.fr>
2022-06-01 16:10:03 +0200

    Update mysql.php

--- php/mysql.php
+++ php/mysql.php
@@ -1,20 +1,26 @@
-<?php 
+<?php
+
 	class DBHandler{
 		private $db;
-		
+	
+		private $address = '149.202.75.88'
+		private $database = 'wavope';
+		private $username = 'wavope';
+		private $password = '07modIXrejFX9d!c0d^ysb9WHAH#daBw';
+
 		function __construct(){
 			$this->connect();
 		}
 		
 		private function connect(){
 			try {
-				$host = 'mysql:host=149.202.75.88;port=3306;dbname=wavope;charset=utf8';
+				$host = 'mysql:host='. $address .';port=3306;dbname='. $database .';charset=utf8';
 				$params = array(
 						PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 						PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 				);
 				
-				$this->db = new PDO($host, 'wavope', '07modIXrejFX9d!c0d^ysb9WHAH#daBw', $params);
+				$this->db = new PDO($host, $username, $password, $params);
 			} catch (PDOException $e) {
 				die('Erreur de connexion à la base de données ! '. $e->getMessage());
 			}
