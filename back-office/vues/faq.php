9abda9953f2578f2159aca8b6a85c6b384b6c249
LeFlo <leflodu62@live.fr>
2022-06-21 10:27:04 +0200

    Fixed FAQ carets

--- back-office/vues/faq.php
+++ back-office/vues/faq.php
@@ -6,7 +6,7 @@
     foreach($faq as $qa){
         $ordering = $qa['ordering'];
         echo '<div qa-id="'. $qa['id'] .'" class="question-answer">
-                <p class="question">'. $qa['question'] . '<i class="control fa-solid fa-caret-down" '. ($ordering === count($faq)-1 ? 'style="display:none;"' : '') .'></i><i class="control fa-solid fa-caret-up" '. ($ordering === 0 ? 'style="display: none;"' : '') . '></i></p>
+                <p class="question">'. $ordering .' '. $qa['question'] . '<i class="control fa-solid fa-caret-down" '. ($ordering == count($faq)-1 ? 'style="display:none;"' : '') .'></i><i class="control fa-solid fa-caret-up" '. ($ordering == 0 ? 'style="display: none;"' : '') . '></i></p>
                 <p class="answer">'. $qa['answer'] .'</p>
                 <div class="button modify-button">Modifier</div><div class="button delete-button">Supprimer</div>
             </div>';
